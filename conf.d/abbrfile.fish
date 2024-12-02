7ce641c3e60f923c54fb0fd686c865d60beaa562
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2024-12-02 14:29:27 -0800

    Update abbrfile.fish
    Update; location of abbrs file

--- conf.d/abbrfile.fish
+++ conf.d/abbrfile.fish
@@ -3,29 +3,29 @@
 #= Create abbreviations for every entry in `abbreviations`.
 #= [github/jabirali/ fish-abbrfile](https://github.com/jabirali/fish-abbrfile)
 #= > [github/jabirali/ fish-abbrfile](https://github.com/LaurentFough/fish-abbrfile)
-if [ -f $FDOTDIR/fish_abbreviations.fish ]
-	for line in (sed '/^#/d' -f $FDOTDIR/fish_abbreviations.fish)
-		set -l dict (string split ' ' -- $line)
-		if [ (count $dict) -ge 2 ]
-      #= Abbreviation that should trigger expansion.
+if test -f {$FDOTDIR}/abbrs.d/__abbreviations.fish;
+	for line in ( sed '/^#/d' -f {$FDOTDIR}/abbrs.d/__abbreviations.fish )
+		set -l dict ( string split ' ' -- $line )
+		if [ ( count $dict ) -ge 2 ]
+		#= Abbreviation that should trigger expansion.
 			set -l key (string trim -- $dict[1])
-      #= Command to run when the triggering happens.
+		#= Command to run when the triggering happens.
 			set -l val (string trim -- (string join ' ' -- $dict[2..-1]))
-      #= Only define abbreviation if the command is valid. This makes it
-      #= possible to e.g. abbreviate `vi` to `vim` on systems where `vim`
-      #= is available, but use the real `vi` if `vim` is not installed.
-      set -l cmd (string split --no-empty ' ' -- $val)
-      if [ $cmd[1] = "sudo" ]
-        #= Sudo detected. Check the next argument to see how to proceed.
-        if type -q $cmd[2]
-          abbr -ga $key $val
-        end
-      else
-        #= Sudo not detected. Just check the first argument of the command.
-        if type -q $cmd[1]
-          abbr -ga $key $val
-        end
-      end
+		#= Only define abbreviation if the command is valid. This makes it
+		#= possible to e.g. abbreviate `vi` to `vim` on systems where `vim`
+		#= is available, but use the real `vi` if `vim` is not installed.
+		set -l cmd (string split --no-empty ' ' -- $val)
+		if [ $cmd[1] = "sudo" ]
+		#= Sudo detected. Check the next argument to see how to proceed.
+		if type -q $cmd[2]
+			abbr -ga $key $val
+		end
+		else
+		#= Sudo not detected. Just check the first argument of the command.
+		if type -q $cmd[1]
+			abbr -ga $key $val
+		end
+		end
 		else if [ -n "$line" ]
 			echo "abbrfile: Could not parse \"$line\"."
 		end
